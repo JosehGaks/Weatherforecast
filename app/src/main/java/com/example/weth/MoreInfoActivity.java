1cc00665a68b1f8ada51982851b9c2b73ae1a025
JosehGaks <josehwilddog@gmail.com>
2020-03-19 08:52:41 +0300

    create an intent on list item click event

---
+++ app/src/main/java/com/example/weth/MoreInfoActivity.java
@@ -0,0 +1,97 @@
+package com.example.weth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MoreInfoActivity extends AppCompatActivity {
+    char degrees = '\u00B0';
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_info);
+
+
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        if (extras != null){
+                double maxTemperature = extras.getDouble("maxTemp");
+                double minTemperature = extras.getDouble("minTemp");
+               int imageRes = extras.getInt("imageResource");
+
+                Integer humidity = extras.getInt("humidity");
+                int pressure = extras.getInt("pressure");
+                String description = extras.getString("description");
+
+            Log.e("moreinfo",String.valueOf(imageRes));
+                TextView desc =  (TextView) findViewById(R.id.description);
+                desc.setText(description);
+
+                ImageView icon  =  (ImageView) findViewById(R.id.wet_icon);
+                icon.setImageResource(getIconResourceForWeatherCondition(imageRes));
+
+                TextView humid =  (TextView) findViewById(R.id.humidity);
+                humid.setText(String.valueOf(humidity)+ " %");
+
+                TextView press = (TextView)  findViewById(R.id.pressure);
+                press.setText(String.valueOf(pressure)+" hPa");
+
+                TextView maxT = (TextView) findViewById(R.id.maxtemp);
+                maxT.setText(formatTemp(maxTemperature));
+
+                TextView minT =  (TextView) findViewById(R.id.mintemp);
+                minT.setText(formatTemp(minTemperature));
+            }
+        }
+
+    private String formatTemp(double temperature){
+        double newTemperature = temperature - 273.15;
+        DecimalFormat magnitudeFormat = new DecimalFormat("0");
+        return magnitudeFormat.format(newTemperature)+degrees;
+
+    }
+
+
+
+
+
+    private int getIconResourceForWeatherCondition(int weatherId) {
+        /*
+         * Based on weather code data found at:
+         * See http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes
+         */
+        if (weatherId >= 200 && weatherId <= 232) {
+            return R.drawable.ic_storm;
+        } else if (weatherId >= 300 && weatherId <= 321) {
+            return R.drawable.ic_light_rain;
+        } else if (weatherId >= 500 && weatherId <= 504) {
+            return R.drawable.ic_rain;
+        } else if (weatherId == 511) {
+            return R.drawable.ic_snow;
+        } else if (weatherId >= 520 && weatherId <= 531) {
+            return R.drawable.ic_rain;
+        } else if (weatherId >= 600 && weatherId <= 622) {
+            return R.drawable.ic_snow;
+        } else if (weatherId >= 701 && weatherId <= 761) {
+            return R.drawable.ic_fog;
+        } else if (weatherId == 761 || weatherId == 781) {
+            return R.drawable.ic_storm;
+        } else if (weatherId == 800) {
+            return R.drawable.ic_clear;
+        } else if (weatherId == 801) {
+            return R.drawable.ic_light_clouds;
+        } else if (weatherId >= 802 && weatherId <= 804) {
+            return R.drawable.ic_cloudy;
+        }
+        return -1;
+    }
+}
